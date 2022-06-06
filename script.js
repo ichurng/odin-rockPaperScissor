7945da9497d04ae525a20c92b830fe3bd324380e
I Chun Hurng <ichunhurng@gmail.com>
2022-06-06 14:15:07 -0700

    Add computerPlay() that outputs a random number from 0 to 2, inclusive

---
+++ script.js
@@ -0,0 +1,14 @@
+function computerPlay(){
+    let computerChoice = Math.floor(Math.random() * 3);
+    return computerChoice;
+}
+
+function playerSelection(){
+
+}
+
+function computerSelection(){
+
+}
+
+console.log(computerPlay());
