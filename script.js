7fe951f024dd50e67b9f3b310154e7b427d9a3ed
I Chun Hurng <ichunhurng@gmail.com>
2022-06-06 14:19:50 -0700

    Change return value of computerChoice() from random integer to strings representing rock, paper, or scissor

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function computerPlay(){
-    let computerChoice = Math.floor(Math.random() * 3);
+    let randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice;
+    if (randomNumber == 0){
+        computerChoice = "Rock";
+    }
+    else if (randomNumber == 1){
+        computerChoice = "Paper";
+    }
+    else if (randomNumber == 2){
+        computerChoice = "Scissor";
+    }
     return computerChoice;
 }
 
