a959904e3fe77a2e4f87516fe33cf951783eebe2
I Chun Hurng <ichunhurng@gmail.com>
2022-06-07 21:16:11 -0700

    Refactor code to implement graphical user interface

--- script.js
+++ script.js
@@ -2,64 +2,87 @@ function computerSelection(){
     let randomNumber = Math.floor(Math.random() * 3);
     let computerChoice = randomNumber;
     return computerChoice;
-    /*
-    if (randomNumber == 0){
-        computerChoice = "Rock";
-    }
-    else if (randomNumber == 1){
-        computerChoice = "Paper";
-    }
-    else if (randomNumber == 2){
-        computerChoice = "Scissor";
-    }
-    */
     
 }
 
-function playerSelection(){
-    let playerChoice = prompt("Please enter Rock, Paper, or Scissor");
-    playerChoice = playerChoice.toUpperCase();
+function playerSelection(playerChoice){
+    // let playerChoice = prompt("Please enter Rock, Paper, or Scissor");
     let playerChoiceNumber;
     if (playerChoice == "ROCK"){
         playerChoiceNumber = 0;
     }
-    else if(playerChoice == "PAPER"){
+    else if (playerChoice == "PAPER"){
         playerChoiceNumber = 1;
     }
-    else if(playerChoice == "SCISSOR"){
+    else if (playerChoice == "SCISSOR"){
         playerChoiceNumber = 2;
     }
+
     return playerChoiceNumber;
 }
 
-function playRound(){
-    let playerChoice = playerSelection();
+
+function playRound(playerShoot){
+    let playerChoice = playerSelection(playerShoot);
     let computerChoice = computerSelection();
-    console.log(playerChoice);
-    console.log(computerChoice);
+
     if (playerChoice == 0 && computerChoice == 2){
-        console.log("You win!");
+        return 1;
     }
     else if (playerChoice == 2 && computerChoice == 0){
-        console.log("You lose!");
+        return -1;
     }
     else if (playerChoice == computerChoice){
-        console.log("Its a tie!");
+        return 0;
     }
     else if (playerChoice > computerChoice){
-        console.log("You win!");
+        return 1;
     }
     else{
-        console.log("You lose!");
+        return -1;
     }
 }
 
-/*
-function game(){
-    for (let i=0; i<5; i++){
-        playRound();
-    }
-}
 
-game();
-*/
+var playerScore = 0;
+var computerScore = 0;
+let container = document.createElement('div');
+let text = document.createTextNode(`"Your Score: ${playerScore} \n Computer Score: ${computerScore}"`);
+container.appendChild(text);
+document.body.appendChild(container);
+
+document.body.addEventListener('click', (e) => {
+    if (e.target.nodeName == 'BUTTON'){
+        let playerChoice = e.target.className;
+        let result = playRound(playerChoice);
+        if (result == 1){
+            playerScore += 1;
+        }
+        else if (result == -1){
+            computerScore += 1;
+        }
+        text.nodeValue = `"Your Score: ${playerScore} \n Computer Score: ${computerScore}"`;
+    }
+
+    if (playerScore == 5){
+        text.nodeValue = "You win!";
+
+    }
+    else if (computerScore == 5){
+        text.nodeValue = "You lose :(";
+        playerScore = 0;
+        computerScore = 0;
+    }
+})
+
+
+
+
+
+
+
+    
+
+       
+
+
